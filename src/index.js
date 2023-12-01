3e83df652699283797cfe27bb86d44981fc19585
Cody Bennett <hi@codyb.co>
2023-12-01 06:42:37 -0600

    WIP Hi-Z cull

--- src/index.js
+++ src/index.js
@@ -85,16 +85,16 @@ THREE.WebGLRenderer.prototype.compute = function (node) {
   node.material = oldMaterial
 
   // Debug CPU readback
-  // for (const output of compiled.outputs) {
-  //   const attribute = node.geometry.attributes[output]
-  //   const { buffer } = this.attributes.get(attribute)
+  for (const output of compiled.outputs) {
+    const attribute = node.geometry.attributes[output]
+    const { buffer } = this.attributes.get(attribute)
 
-  //   gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
-  //   gl.getBufferSubData(gl.ARRAY_BUFFER, 0, attribute.array)
-  //   gl.bindBuffer(gl.ARRAY_BUFFER, null)
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+    gl.getBufferSubData(gl.ARRAY_BUFFER, 0, attribute.array)
+    gl.bindBuffer(gl.ARRAY_BUFFER, null)
 
-  //   console.log(output, Array.from(attribute.array))
-  // }
+    console.log(output, Array.from(attribute.array)[0])
+  }
 }
 
 THREE.ShaderMaterial.prototype.computeShader = ''
@@ -105,6 +105,8 @@ document.body.appendChild(renderer.domElement)
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight)
 camera.position.z = 5
+// camera.position.x = 4
+// camera.lookAt(0, 0, 0)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
@@ -114,51 +116,108 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BufferGeometry()
 geometry.setDrawRange(0, 3)
 geometry.boundingSphere = new THREE.Sphere().set(new THREE.Vector3(), Infinity)
-geometry.setAttribute('visibility', new THREE.BufferAttribute(new Int32Array(3), 1))
-geometry.attributes.visibility.gpuType = THREE.IntType
+// geometry.setAttribute('visibility', new THREE.BufferAttribute(new Int32Array(3), 1))
+// geometry.attributes.visibility.gpuType = THREE.IntType
+geometry.setAttribute('console', new THREE.BufferAttribute(new Float32Array(3), 1))
 
 const projectionViewMatrix = new THREE.Matrix4()
 
 const material = new THREE.RawShaderMaterial({
   uniforms: {
+    resolution: new THREE.Uniform(new THREE.Vector2()),
     projectionViewMatrix: new THREE.Uniform(projectionViewMatrix),
+    mipmaps: new THREE.Uniform(null),
+    radius: new THREE.Uniform(),
   },
   computeShader: /* glsl */ `//#version 300 es
+    uniform vec2 resolution;
     uniform mat4 projectionViewMatrix;
+    uniform sampler2D[6] mipmaps;
+    uniform float radius;
 
-    flat out int visibility;
+    out float console;
 
-    const float radius = 1.0;
+    // const float radius = 0.87;
     const vec4 position = vec4(0, 0, 0, 1);
 
-    void main() {
-      // http://cs.otago.ac.nz/postgrads/alexis/planeExtraction.pdf
-      vec4 planes[] = vec4[](
-        projectionViewMatrix[3] - projectionViewMatrix[0], // left   (-w < +x)
-        projectionViewMatrix[3] + projectionViewMatrix[0], // right  (+x < +w)
-        projectionViewMatrix[3] - projectionViewMatrix[1], // bottom (-w < +y)
-        projectionViewMatrix[3] + projectionViewMatrix[1], // top    (+y < +w)
-        projectionViewMatrix[3] - projectionViewMatrix[2], // near   (-w < +z)
-        projectionViewMatrix[3] + projectionViewMatrix[2]  // far    (+z < +w)
+    vec4 textureGather(sampler2D tex, vec2 uv, int comp) {
+      vec2 res = vec2(textureSize(tex, 0));
+      ivec2 p = ivec2((uv * res) - 0.5);
+      return vec4(
+        texelFetchOffset(tex, p, 0, ivec2(0, 1))[comp],
+        texelFetchOffset(tex, p, 0, ivec2(1, 1))[comp],
+        texelFetchOffset(tex, p, 0, ivec2(1, 0))[comp],
+        texelFetchOffset(tex, p, 0, ivec2(0, 0))[comp]
       );
+    }
 
-      visibility = 0;
-      for (int i = 0; i < 6; i++) {
-        float distance = dot(planes[i], position);
-        if (distance <= -radius) {
-          visibility = 2;
-          break;
+    void main() {
+      bool visible = true;
+
+      // Frustum cull
+      if (false) {
+        // http://cs.otago.ac.nz/postgrads/alexis/planeExtraction.pdf
+        vec4 planes[] = vec4[](
+          projectionViewMatrix[3] - projectionViewMatrix[0], // left   (-w < +x)
+          projectionViewMatrix[3] + projectionViewMatrix[0], // right  (+x < +w)
+          projectionViewMatrix[3] - projectionViewMatrix[1], // bottom (-w < +y)
+          projectionViewMatrix[3] + projectionViewMatrix[1], // top    (+y < +w)
+          projectionViewMatrix[3] - projectionViewMatrix[2], // near   (-w < +z)
+          projectionViewMatrix[3] + projectionViewMatrix[2]  // far    (+z < +w)
+        );
+
+        for (int i = 0; i < 6; i++) {
+          float distance = dot(planes[i], position);
+          if (distance <= -radius) {
+            visible = false;
+            break;
+          }
         }
       }
+
+      console = -1.0;
+
+      // Occlusion cull
+      if (visible) {
+        // Calculate NDC from projected position
+        vec4 ndc = transpose(projectionViewMatrix) * vec4(position.xy, position.z - radius, 1);
+        ndc.xyz /= ndc.w;
+
+        // Calculate screen-space UVs
+        vec2 uv = (ndc.xy + 1.0) * 0.5;
+
+        // Calculate Hi-Z mip
+        int mip = clamp(int(ceil(log2(radius * resolution))), 0, 5);
+        mip = 0; // TODO: safer clamp
+
+        // Calculate max depth
+        vec4 tile;
+        if (mip == 0) tile = textureGather(mipmaps[0], uv, 0);
+        if (mip == 1) tile = textureGather(mipmaps[1], uv, 0);
+        if (mip == 2) tile = textureGather(mipmaps[2], uv, 0);
+        if (mip == 3) tile = textureGather(mipmaps[3], uv, 0);
+        if (mip == 4) tile = textureGather(mipmaps[4], uv, 0);
+        if (mip == 5) tile = textureGather(mipmaps[5], uv, 0);
+        float depth = max(max(tile.x, tile.y), max(tile.z, tile.w));
+
+        console = ndc.z;
+
+        // Test against conservative depth
+        if (abs(depth - ndc.z) > 0.01 && depth > ndc.z) visible = false;
+      }
+
+      // Write visibility
+      // visibility = visible ? 0 : 2;
     }
   `,
   vertexShader: /* glsl */ `//#version 300 es
     out vec2 vUv;
-    in int visibility;
+    // in int visibility;
+    in float console;
 
     void main() {
       vUv = vec2(gl_VertexID << 1 & 2, gl_VertexID & 2);
-      gl_Position = vec4(vUv * 2.0 - 1.0, visibility, 1);
+      gl_Position = vec4(vUv * 2.0 - 1.0, 0, 1);
     }
   `,
   fragmentShader: /* glsl */ `//#version 300 es
@@ -175,17 +234,96 @@ const material = new THREE.RawShaderMaterial({
   glslVersion: THREE.GLSL3,
 })
 const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+// scene.add(mesh)
 
-scene.add(new THREE.Mesh(new THREE.BoxGeometry(), new THREE.MeshNormalMaterial()))
+const blitMaterial = new THREE.ShaderMaterial({
+  uniforms: {
+    tDepth: new THREE.Uniform(null),
+    cameraNear: new THREE.Uniform(camera.near),
+    cameraFar: new THREE.Uniform(camera.far),
+  },
+  vertexShader: /* glsl */ `
+    out vec2 vUv;
+
+    void main() {
+      vUv = vec2(gl_VertexID << 1 & 2, gl_VertexID & 2);
+      gl_Position = vec4(vUv * 2.0 - 1.0, 0, 1);
+    }
+  `,
+  fragmentShader: /* glsl */ `
+    uniform sampler2D tDepth;
+    uniform float cameraNear;
+    uniform float cameraFar;
+    in vec2 vUv;
+
+    #include <packing>
+
+    vec4 textureGather(sampler2D tex, vec2 uv, int comp) {
+      vec2 res = vec2(textureSize(tex, 0));
+      ivec2 p = ivec2((uv * res) - 0.5);
+      return vec4(
+        texelFetchOffset(tex, p, 0, ivec2(0, 1))[comp],
+        texelFetchOffset(tex, p, 0, ivec2(1, 1))[comp],
+        texelFetchOffset(tex, p, 0, ivec2(1, 0))[comp],
+        texelFetchOffset(tex, p, 0, ivec2(0, 0))[comp]
+      );
+    }
+
+    void main() {
+      vec4 tile = textureGather(tDepth, vUv, 0);
+      gl_FragDepth = max(max(tile.x, tile.y), max(tile.z, tile.w));
+
+      float viewZ = perspectiveDepthToViewZ(gl_FragDepth, cameraNear, cameraFar);
+      float depth = viewZToOrthographicDepth(viewZ, cameraNear, cameraFar);
+      gl_FragColor = vec4(vec3(1.0 - depth), 1);
+    }
+  `,
+})
+const blitMesh = new THREE.Mesh(geometry, blitMaterial)
+
+const cube = new THREE.Mesh(new THREE.BoxGeometry(), new THREE.MeshNormalMaterial())
+cube.geometry.computeBoundingSphere()
+material.uniforms.radius.value = cube.geometry.boundingSphere.radius
+scene.add(cube)
+
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(), new THREE.MeshNormalMaterial())
+plane.position.z = 1
+plane.scale.setScalar(2)
+plane.material.transparent = true
+plane.material.opacity = 0.2
+// scene.add(plane)
+
+const renderTarget = new THREE.WebGLRenderTarget()
+renderTarget.depthBuffer = true
+renderTarget.depthTexture = new THREE.DepthTexture()
 
 const onResize = () => {
+  material.uniforms.resolution.value.set(window.innerWidth, window.innerHeight)
   renderer.setSize(window.innerWidth, window.innerHeight)
+  renderTarget.setSize(window.innerWidth, window.innerHeight)
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
 }
 onResize()
 window.addEventListener('resize', onResize)
+
+let width = window.innerWidth
+let height = window.innerHeight
+const numLevels = Math.min(6, 1 + Math.floor(Math.log2(Math.max(width, height))))
+
+const mipmaps = [renderTarget]
+for (let i = 1; i < numLevels; i++) {
+  width = Math.max(1, Math.floor(width * 0.5))
+  height = Math.max(1, Math.floor(height * 0.5))
+
+  const mipmap = new THREE.WebGLRenderTarget(width, height)
+  mipmap.depthBuffer = true
+  mipmap.depthTexture = new THREE.DepthTexture()
+
+  mipmaps[i] = mipmap
+}
+
+material.uniforms.mipmaps.value = mipmaps.map((mipmap) => mipmap.depthTexture)
 
 renderer.setAnimationLoop(() => {
   controls.update()
@@ -193,6 +331,20 @@ renderer.setAnimationLoop(() => {
   camera.updateWorldMatrix()
   projectionViewMatrix.multiplyMatrices(camera.projectionMatrix, camera.matrixWorldInverse).transpose()
 
-  renderer.compute(mesh)
+  renderer.setRenderTarget(renderTarget)
   renderer.render(scene, camera)
+
+  for (let i = 1; i < numLevels; i++) {
+    blitMaterial.uniforms.tDepth.value = mipmaps[i - 1].depthTexture
+    renderer.setRenderTarget(mipmaps[i])
+    renderer.render(blitMesh, camera)
+  }
+
+  // blitMaterial.uniforms.tDepth.value = renderTarget.depthTexture
+
+  renderer.setRenderTarget(null)
+  renderer.render(blitMesh, camera)
+
+  renderer.compute(mesh)
+  // renderer.render(mesh, camera)
 })
